df498e54f2f8b847e4e75d33fa678a2bf558233c
Tolu Akinnubi <tolu.akinnubi@yahoo.com>
2022-07-14 01:41:23 +0100

    feat: ui change and mapbox integration.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import Landing from './components/Landing';
 
 function App() {
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,18 +1,30 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import mapboxgl from 'mapbox-gl';
 
 function Landing()
 {
-
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [geo1, setGeo1] = useState([-117.985904, 35.125801]);
+    const [geo2, setGeo2] = useState([-115.148516, 36.167256]);
+    const zoom = 5.5;
+    
     let [averageMonthlySavings, setAverageMonthlySavings] = useState(); 
     let [averageYearlySavings, setAverageYearlySavings] = useState();
     let [cheaperCity, setCheaperCity] = useState();
-    let [results, setResults] = useState("No Results available");
+    let [results, setResults] = useState("No results available to be displayed.");
     let [cities, setCities] = useState([]);
-
+    let [cityOne, setCityOne] = useState("");
+    let [cityOneGrade, setCityOneGrade] = useState("");
+    let [cityTwo, setCityTwo] = useState("");
+    let [cityTwoGrade, setCityTwoGrade] = useState("");
+    let [safetyTitle, setSafetyTitle] = useState("");
+    let [savingTitle, setSavingTitle] = useState("");
+    
     async function getCities()
     {
         try{
-
+            
             const cityResponse = await fetch('https://comparison-appx.herokuapp.com/cities');
             const cityData = await cityResponse.json();
             // console.log(cityData);
@@ -21,26 +33,25 @@ function Landing()
             console.log(err);
         }
     }
-
+    
     async function compareCities(event)
     {
         event.preventDefault();
-
-        let cityOne = event.target[0].value;
-        let cityTwo = event.target[1].value;
+        
+        cityOne = event.target[0].value;
+        cityTwo = event.target[1].value;
         let beds = event.target[2].value;
         let baths = event.target[3].value;
-
+        
         if(baths.indexOf('.') === -1){
             baths += ".0";
         }
-        //console.log(baths.indexOf('.'));
-
+        
         try{
             const response = await fetch('https://comparison-appx.herokuapp.com/search', {
                 method: 'POST',
                 headers: {
-                'Content-Type': 'application/json'
+                    'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
                     cityOne: cityOne,
@@ -49,123 +60,216 @@ function Landing()
                     beds: beds
                 })
             });
+
+            const responseGeo1 = await fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/'+cityOne+', california, usa.json?access_token=pk.eyJ1IjoieWlzaG1hZWwiLCJhIjoiY2w0eWFoczlsMDhrYjNkdWx0dmZydTl1bSJ9.S0jJfE96iZyxxVlIanDmvQ');
+            const responseGeo2 = await fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/'+cityTwo+', california, usa.json?access_token=pk.eyJ1IjoieWlzaG1hZWwiLCJhIjoiY2w0eWFoczlsMDhrYjNkdWx0dmZydTl1bSJ9.S0jJfE96iZyxxVlIanDmvQ');
+
+            const geoData1 = await responseGeo1.json();
+            const geoData2 = await responseGeo2.json();
+            console.log(geoData1.features[0].center);
+            setGeo1(geoData1.features[0].center);
+            setGeo2(geoData2.features[0].center);
+
             const data = await response.json();
-
-                if(data.message){
-                    let msg = data.message;
-                    if(msg === "city does not exsist") {
-                        msg = "Sorry, we are yet to have the data.";
-                    }
-                    setResults(msg); 
-                    setAverageYearlySavings("");
-                    setCheaperCity("");
-                    setAverageMonthlySavings("");   
-                }else{
-                    setResults("All results"); 
-                    setAverageYearlySavings("Average yearly savings: $"+data.AverageYearlySavings)
-                    setCheaperCity("Cheaper city: "+data.CheaperCity)
-                    setAverageMonthlySavings("Average monthly savings: $"+data.AverageMonthlySavings)
+            setResults(""); 
+            setAverageYearlySavings("");
+            setCheaperCity("");
+            setAverageMonthlySavings("");
+            setCityOne("");   
+            setCityTwo("");
+            setCityOneGrade("");
+            setCityTwoGrade("");  
+            setSavingTitle(""); 
+            setSafetyTitle(""); 
+            if(data.message){
+                let msg = data.message;
+                if(msg === "city does not exsist" || data.citySavings.AverageMonthlySavings === null) {
+                    msg = "Sorry, we are yet to have the data.";
                 }
-
-            //console.log(data);
+                setResults(msg); 
+                
+            }else{
+                if(data.citySavings.AverageMonthlySavings === null) {
+                    setResults("Sorry, we are yet to have the data.");
+                }else{
+                    
+                    setResults(""); 
+                    setSavingTitle("Cost Savings"); 
+                    setSafetyTitle("Safety Grade"); 
+                    setAverageYearlySavings("Average yearly savings: $"+data.citySavings.AverageYearlySavings);
+                    setCheaperCity("Cheaper city: "+data.citySavings.CheaperCity.charAt(0).toUpperCase() + data.citySavings.CheaperCity.slice(1));
+                    setAverageMonthlySavings("Average monthly savings: $"+data.citySavings.AverageMonthlySavings);
+                    setCityOne(data.citySavings.CityOne);
+                    setCityTwo(data.citySavings.CityTwo);
+                    setCityOneGrade(data.cityOneSafety.crime_grade);
+                    setCityTwoGrade(data.cityTwoSafety.crime_grade);
+                }
+            }
+            
         }catch(err){
             console.error(err);
         }
     }
-
+    
+    mapboxgl.accessToken = 'pk.eyJ1IjoieWlzaG1hZWwiLCJhIjoiY2w0eWFoczlsMDhrYjNkdWx0dmZydTl1bSJ9.S0jJfE96iZyxxVlIanDmvQ';
+    
     useEffect(() => {
         getCities();
-    }, []);
+        generateMap(map, mapContainer, geo1, geo2, zoom, cityOne, cityTwo);
+        
+            
+    }, [geo1, geo2, zoom, cityOne, cityTwo]);
+
+    async function generateMap(map, mapContainer, geo1, geo2, zoom, cityOne, cityTwo) {
+
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: geo1,
+            zoom: zoom
+        });
+
+        map.current.on('load', function () {
+
+            map.current.addLayer({
+                id: "route",
+                type: "line",
+                    source: {
+                        type: "geojson",
+                        data: {
+                            type: "Feature",
+                            properties: {},
+                            geometry: {
+                                type: "LineString",
+                                coordinates: [geo1, geo2 ]
+                            }
+                        }
+                    }
+            });
+
+            map.current.fitBounds([geo1,geo2], { padding: 250 });
+            
+        });
+
+        const popup1 = new mapboxgl.Popup()
+        .setText(cityOne.charAt(0).toUpperCase() + cityOne.slice(1)).addTo(map.current);
+
+        const popup2 = new mapboxgl.Popup()
+        .setText(cityTwo.charAt(0).toUpperCase() + cityTwo.slice(1)).addTo(map.current);
+
+        new mapboxgl.Marker({ "color": "#BB2D3A" }).setLngLat(geo1).addTo(map.current).setPopup(popup1);
+        new mapboxgl.Marker({ "color": "#202A44" }).setLngLat(geo2).addTo(map.current).setPopup(popup2);
+    
+        return () => map.current.remove();
+
+    }
+
 
     return(
-        <div className="container pt-5 text-center">
-            <h1>Hi there!</h1>
-                <br />
-                <br />
-                   <h4>Are you looking to relocate?</h4> 
-                <p className="text-center">
-                   Would you like to see what you would be saving?
-                </p>
+        <div className="container-fluid m-0">
+            
+            <div className="container-fluid m-0">
+                
                 <div className="row">
-                    <div className="col-md-2"></div>
-                    <div className="col-md-8">
-                        <form action="" onSubmit={(e)=>{ compareCities(e); }}>
 
-                            <div className="row g-3">
+                    <div className="col-md-2 m-0">
 
-                                <div className="col-sm text-start">
-                                    <label htmlFor="cityOne"><b>City One:</b></label>
-                                    <select required className="form-control" name="cityOne">
-                                        <option value="">select</option>
-                                       { cities.map((city, index)=>  (<option value={city} key={index}>{city.charAt(0).toUpperCase() + city.slice(1)}</option>) ) }
-                                    </select>
-                                </div>
+                        <h3 className="pt-3 px-3 text-blue">Comparison<span className="text-red">.</span></h3>
+                        <br /><br />
+                        
+                        <form className="pb-5" action="" onSubmit={(e)=>{ compareCities(e); }}>
 
-                                <div className="col-sm text-start">
-                                    <label htmlFor="cityTwo"><b>City Two:</b></label>
-                                    <select required className="form-control" name="cityTwo">
-                                        <option value="">select</option>
-                                       { cities.map((city, index)=>  (<option value={city} key={index}>{city.charAt(0).toUpperCase() + city.slice(1)}</option>) ) }
-                                    </select>
-                                </div>
-
-                                <div className="col-sm text-start">
-                                    <label htmlFor="beds"><b>Beds:</b></label>
-                                    <input required type="number" className="form-control" name="beds" min="0" placeholder="Total Beds" aria-label="Beds"/>
-                                </div>
-
-                                <div className="col-sm text-start">
-                                    <label htmlFor="baths"><b>Baths:</b></label>
-                                    <input required type="number" className="form-control" name="baths" min="0" placeholder="Total Baths" aria-label="Baths" step="0.5"/>
-                                </div>
-
-                                <div className="col-sm mt-auto">
-                                    <input type="submit" className="btn btn-dark " value="Submit"/>
-                                </div>
-                                
+                            <div>
+                                <label  className="small text-blue" htmlFor="cityOne"><b>City One: </b></label>
+                                <select required className="form-control" name="cityOne">
+                                    <option value="">select</option>
+                                    { cities.map((city, index)=>  (<option value={city} key={index}>{city.charAt(0).toUpperCase() + city.slice(1)}</option>) ) }
+                                </select>
                             </div>
 
+                            <br />
+                            <div>
+                                <label  className="small text-blue" htmlFor="cityTwo"><b>City Two: </b></label>
+                                <select required className="form-control" name="cityTwo">
+                                    <option value="">select</option>
+                                    { cities.map((city, index)=>  (<option value={city} key={index}>{city.charAt(0).toUpperCase() + city.slice(1)}</option>) ) }
+                                </select>
+                            </div>
+
+                            <br />
+                            <div>
+                                <label className="small text-blue" htmlFor="beds"><b>Beds: </b></label>
+                                <input required type="number" className="form-control" name="beds" min="0" placeholder="Beds" aria-label="Beds"/>
+                            </div>
+
+                            <br />
+                            <div >
+                                <label className="small text-blue" htmlFor="baths"><b>Baths: </b></label>
+                                <input required type="number" className="form-control" name="baths" min="0" placeholder="Baths" aria-label="Baths" step="0.5"/>
+                            </div>
+
+                            <br />
+                            <div className="col-sm-2 text-center mt-auto">
+                                <input type="submit" className="btn btn-danger rounded-pill px-4" value="Submit"/>
+                            </div>
+
+
                         </form>
-                    </div>
-                    <div className="col-md-2"></div>
-                 </div>
 
+                    </div>
+
+                    <div className="col-md-10 p-0 m-0 pr-2">
+                        <div ref={mapContainer} className="mapFrame"></div>
+                        {/* <iframe title="map-view" className="mapFrame" id="gmap_canvas" src={"https://maps.google.com/maps?q="+CityOne+", ca&t=&z=12&ie=UTF8&iwloc=&output=embed"} frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe> */}
+                    </div>
+
+                    
+
+                </div>
+            </div>
+
+            <div className="cityResult">
+                <h5 className="text-blue text-center">Results</h5>
+                
+                <div id="savings">
+                    { results.charAt(0).toUpperCase() + results.slice(1) }
+                    
+                    <b className="text-red">{ savingTitle }</b>
+                    <br />
+                    <small>{ cheaperCity }</small>
+                    <br />
+                    <small>{ averageMonthlySavings }</small>
+                    <br />
+                    <small>{ averageYearlySavings }</small>
+                </div>
                 <br />
-                 {/* <div className="row">
-                    <div className="col-md-1"></div>
-
-                    <div className="col-md-2">
-                        <label htmlFor="pets_friendly">Pets Friendly</label>
-                    </div>
-                    <div className="col-md-2">
-                        <label htmlFor="laundry_options">Laundry Options</label>
-                    </div>
-                    <div className="col-md-2">
-                        <label htmlFor="parking_options">Parking Options</label>
-                    </div>
-
-                    <div className="col-md-2">
-                        <label htmlFor="parking_options">Smoking Allowed</label>
+                <div id="safety" className="">
+                    <b className="text-red">{ safetyTitle }</b>
+                    <br />
+                    <div className="d-flex">
+                        <div>
+                            <small>{ cityOne.charAt(0).toUpperCase() + cityOne.slice(1) }</small>  
+                        </div>
+                        <div>
+                            <b className="px-2">{ cityOneGrade }</b>
+                        </div>
                     </div>
 
-                    <div className="col-md-2">
-                        <label htmlFor="parking_options">Funished</label>
+                    <div className="d-flex">
+                        <div>
+                            <small>{ cityTwo.charAt(0).toUpperCase() + cityTwo.slice(1) }</small> 
+                        </div>
+                        <div>
+                            <b className="px-2">{ cityTwoGrade }</b>
+                        </div>
                     </div>
                     
-                    <div className="col-md-1"></div>
-                 </div> */}
-                
-                <br />
-                <br />
-                <div id="results">
-                    { results.charAt(0).toUpperCase() + results.slice(1) }
-                    <br />
-                    <b>{ cheaperCity }</b>
-                    <br />
-                    <b>{ averageMonthlySavings }</b>
-                    <br />
-                    <b>{ averageYearlySavings }</b>
+                    
+
                 </div>
+                
+
+            </div>
         </div>
     );
 }
